fd8acfb0b7b9b2e54122f5e0ed75049ec43552f8
faiyazvaraliya31-droid <faiyazvaraliya31@gmail.com>
2026-04-14 10:31:13 +0530

    Update app.py

--- Ecommerce_project/app.py
+++ Ecommerce_project/app.py
@@ -1,1 +1,16 @@
+import os
+from flask import Flask, request, render_template
 
+app= Flask(__name__, template_folder='templates')
+products = [
+    {id: 1, "name": 'laptop', 'price': '50000', 'image': 'laptop.jpg'},
+    {id: 2, "name": "smartPhone", "price": '20000', 'image': 'phon.jpg'},
+    {id: 3, 'name': 'Headsphones', 'price': '2,000', 'image': 'audio.jpg'}
+]
+
+@app.route('/')
+def home():
+    return render_template("index.html", items=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
